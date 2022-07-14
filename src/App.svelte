4052b4aa41bb67a0dbb51bf5b079caaf1714f023
Bharadwaj Palakurthy <bharadwajpalakurthy@gmail.com>
2022-07-15 01:01:22 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import cantoLogo from './assets/logo.svg'
+</script>
+
+<main>
+ 
+  <h1>the game of canto</h1>
+
+ <img src={cantoLogo} alt="canto logo">
+</main>
+
+<style>
+ 
+</style>
